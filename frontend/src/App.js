8c25fa3601ae0b135c72e83ee0a0e9f1e22dcc98
Ajithkumar <ajithaktech01@gmail.com>
2021-11-25 15:13:51 +0530

    file

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import FileUploadComponent from './file-upload';
+
+
+
+function App() {
+  return (
+    <div className="App">
+         <FileUploadComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,50 @@
+let express = require('express'),
+    mongoose = require('mongoose'),
+    cors = require('cors'),
+    bodyParser = require('body-parser'),
+    dbConfig = require('./shared/db');
+
+const api = require('./services/user')
+
+// MongoDB Configuration
+mongoose.Promise = global.Promise;
+mongoose.connect(dbConfig.db, {
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+}).then(() => {
+    console.log('Database connected')
+},
+    error => {
+        console.log('Database could not be connected: ' + error)
+    }
+)
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(cors());
+
+app.use('/public', express.static('public'));
+
+app.use('/api', api)
+
+const port = 5900;
+
+app.use((req, res, next) => {
+    
+    setImmediate(() => {
+        next(new Error('Something went wrong'));
+    });
+});
+
+app.use(function (err, req, res, next) {
+    console.error(err.message);
+    if (!err.statusCode) err.statusCode = 500;
+    res.status(err.statusCode).send(err.message);
+});
+
+app.listen(port, () => {
+    console.log('App is running on port: ' + port)
+})
